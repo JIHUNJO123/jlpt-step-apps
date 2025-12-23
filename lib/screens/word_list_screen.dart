2708a9437f729c0566fce85529e1c0554a9b706e
JIHUNJO123 <hoonijo@gmail.com>
2025-12-23 10:46:07 +0900

    Fix: Word navigation in word_list and favorites - pass list copy

--- lib/screens/word_list_screen.dart
+++ lib/screens/word_list_screen.dart
@@ -520,7 +520,7 @@ class _WordListScreenState extends State<WordListScreen> {
                     builder:
                         (context) => WordDetailScreen(
                           word: word,
-                          wordList: _words,
+                          wordList: List<Word>.from(_words),
                           currentIndex: index,
                         ),
                   ),
