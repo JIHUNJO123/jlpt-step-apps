ff50ad0f402b07897cbae099ce9e1619793ffb5a
JIHUNJO123 <jihunjo123@gmail.com>
2025-12-27 23:13:57 +0900

    Skip locked words in detail navigation

--- lib/screens/word_detail_screen.dart
+++ lib/screens/word_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jlpt_vocab_app/l10n/generated/app_localizations.dart';
 import '../db/database_helper.dart';
 import '../models/word.dart';
+import '../services/ad_service.dart';
 import '../services/translation_service.dart';
 
 class WordDetailScreen extends StatefulWidget {
@@ -40,10 +41,22 @@ class _WordDetailScreenState extends State<WordDetailScreen> {
   bool get _canGoNext =>
       _hasNavigation && _currentIndex < widget.wordList!.length - 1;
 
+  // 잠긴 단어인지 확인 (짝수 인덱스 = 2, 4, 6...)
+  bool _isWordLocked(int index) {
+    if (index % 2 == 0) return false;
+    return !AdService.instance.isUnlocked;
+  }
+
   void _goToPrevious() {
     if (_canGoPrevious) {
+      int newIndex = _currentIndex - 1;
+      while (newIndex > 0 && _isWordLocked(newIndex)) {
+        newIndex--;
+      }
+      if (_isWordLocked(newIndex)) return;
+
       setState(() {
-        _currentIndex--;
+        _currentIndex = newIndex;
         _word = widget.wordList![_currentIndex];
         _translatedDefinition = null;
         _translatedExample = null;
@@ -54,8 +67,15 @@ class _WordDetailScreenState extends State<WordDetailScreen> {
 
   void _goToNext() {
     if (_canGoNext) {
+      int newIndex = _currentIndex + 1;
+      while (newIndex < widget.wordList!.length - 1 &&
+          _isWordLocked(newIndex)) {
+        newIndex++;
+      }
+      if (_isWordLocked(newIndex)) return;
+
       setState(() {
-        _currentIndex++;
+        _currentIndex = newIndex;
         _word = widget.wordList![_currentIndex];
         _translatedDefinition = null;
         _translatedExample = null;
